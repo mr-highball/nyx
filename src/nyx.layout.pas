8285cd26d38e40236fc9553a730d80ae6cce4b81
mr-highball <35942565+mr-highball@users.noreply.github.com>
2020-07-03 18:03:06 -0400

    further impl #10 - base implementation done for proportional - stubbed out helper methods

--- src/nyx.layout.pas
+++ src/nyx.layout.pas
@@ -253,24 +253,131 @@ type
   *)
   TNyxLayoutFixedClass = class of TNyxLayoutFixedImpl;
 
-  INyxProportionalBounds = interface
+  { INyxProportionalBounds }
+
+  INyxProportionalBounds = interface(INyxBounds)
     ['{B0A4EDF1-6580-4CD5-AABC-F7CB808904DD}']
+
+    //property methods
+    function GetLeft: Single;
+    function GetTop: Single;
+    procedure SetLeft(const AValue: Single);
+    procedure SetTop(const AValue: Single);
+
+    //properties
+
+    (*
+      controls horizontal position of the element, where 0 (0%) would be the
+      "farthest left" inside of the parent and 1.0 (100%) would be the "farthest right"
+    *)
+    property Left : Single read GetLeft write SetLeft;
+
+    (*
+      controls vertical position of the element, where 0 (0%) would be the
+      "farthest top" inside of the parent and 1.0 (100%) would
+      be the "farthest bottom"
+    *)
+    property Top : Single read GetTop write SetTop;
+
+    //methods
+    function UpdateLeft(const AValue : Single) : INyxProportionalBounds;
+    function UpdateTop(const AValue : Single) : INyxProportionalBounds;
+  end;
+
+  { TNyxProportionalBoundsImpl }
+  (*
+    base proportional bounds implementation
+  *)
+  TNyxProportionalBoundsImpl = class(TNyxBoundsImpl, INyxProportionalBounds)
+  strict private
+    FLeft,
+    FTop: Single;
+  strict protected
+  protected
+    function GetLeft: Single;
+    function GetTop: Single;
+    procedure SetLeft(const AValue: Single);
+    procedure SetTop(const AValue: Single);
+  public
+    property Left : SIngle read GetLeft write SetLeft;
+    property Top : Single read GetTop write SetTop;
+
+    function UpdateLeft(const AValue : Single) : INyxProportionalBounds;
+    function UpdateTop(const AValue : Single) : INyxProportionalBounds;
+
+    constructor Create; override;
   end;
 
   { INyxLayoutProportional }
   (*
-
+    a layout to control the placement of elements via percentages
   *)
-  INyxLayoutProportional = interface(INyxBounds)
+  INyxLayoutProportional = interface(INyxLayout)
     ['{C649AA5F-2D1F-40A0-93CC-D5925B20AC12}']
+    //property methods
+    function GetBounds(const AElement : INyxElement): INyxProportionalBounds;
+
+    //properties
+
+    (*
+      accessor for the bounds of a given element
+    *)
+    property Bounds[const AElement : INyxElement] : INyxProportionalBounds read GetBounds; default;
+
+    //methods
+
+    (*
+      adds an element to have it's placement inside of it's parent container
+      using fixed set of bounds
+    *)
+    function Add(const AElement : INyxElement;
+      const ABounds : INyxProportionalBounds) : INyxLayoutProportional; overload;
+
+    (*
+      removes an element from this layout
+      this method is the same as accessing the element collection and removing
+      from their, however this scopes back to the specialized fixed layout
+    *)
+    function Remove(const AElement : INyxElement) : INyxLayoutProportional;
   end;
 
-  TNyxLayoutProportionalImpl = class(TNyxBoundsImpl, INyxLayoutProportional)
+  { TNyxLayoutProportionalImpl }
+  (*
+    base class for proportional layouts
+  *)
+  TNyxLayoutProportionalImpl = class(TNyxLayoutBaseImpl, INyxLayoutProportional)
   strict private
+    FBounds : INyxElements;
+    FRemoveBoundsID : String;
+
+    procedure RemoveBounds(const AElement : INyxElement;
+      const AEvent : TElementsObserveEvent);
   strict protected
+    function DoUpdatePlacement(out Error: String): Boolean; override;
+
+    (*
+      children proportional layouts need to override this in order to place an
+      element within their parent container provided the bounds
+    *)
+    function DoPlaceElement(const AElement : INyxElement;
+      const ABounds : INyxProportionalBounds; out Error : String) : Boolean; virtual; abstract;
   protected
+    function GetBounds(const AElement : INyxElement): INyxProportionalBounds;
   public
+    property Bounds[const AElement : INyxElement] : INyxProportionalBounds read GetBounds; default;
+
+    function Add(const AElement : INyxElement;
+      const ABounds : INyxProportionalBounds) : INyxLayoutProportional; overload;
+    function Remove(const AElement : INyxElement) : INyxLayoutProportional;
+
+    constructor Create; override;
+    destructor Destroy; override;
   end;
+
+  (*
+    metaclass for proportional layouts
+  *)
+  TNyxLayoutProportionalClass = class of TNyxLayoutProportionalImpl;
 
 (*
   helper function to return a fixed layout
@@ -282,6 +389,16 @@ function NewNyxLayoutFixed : INyxLayoutFixed;
 *)
 function NewNyxFixedBounds : INyxFixedBounds;
 
+(*
+  helper function to return a proportional bounds
+*)
+function NewNyxProportionalBounds : INyxProportionalBounds;
+
+(*
+  helper function to return a proportional layout
+*)
+function NewNyxLayoutProportional : INyxLayoutProportional;
+
 implementation
 uses
 {$IFDEF BROWSER}
@@ -292,6 +409,7 @@ uses
 
 var
   DefaultNyxLayoutFixed : TNyxLayoutFixedClass;
+  DefaultNyxLayoutProportional : TNyxLayoutProportionalClass;
 
 function NewNyxLayoutFixed: INyxLayoutFixed;
 begin
@@ -301,6 +419,161 @@ end;
 function NewNyxFixedBounds: INyxFixedBounds;
 begin
   Result := TNyxFixedBoundsImpl.Create;
+end;
+
+function NewNyxProportionalBounds: INyxProportionalBounds;
+begin
+  Result := TNyxProportionalBoundsImpl.Create;
+end;
+
+function NewNyxLayoutProportional: INyxLayoutProportional;
+begin
+  Result := DefaultNyxLayoutProportional.Create;
+end;
+
+{ TNyxLayoutProportionalImpl }
+
+procedure TNyxLayoutProportionalImpl.RemoveBounds(const AElement: INyxElement;
+  const AEvent: TElementsObserveEvent);
+var
+  I: Integer;
+begin
+  //use the element to find the bounds and remove if exists
+  FBounds.IndexOf(AElement, I);
+
+  if (AEvent = eoExtract) and (I >= 0) then
+    FBounds.Delete(AElement);
+end;
+
+function TNyxLayoutProportionalImpl.DoUpdatePlacement(out Error: String): Boolean;
+var
+  I, J: Integer;
+  LElement: INyxElement;
+  LBound: INyxProportionalBounds;
+begin
+  try
+    Result := False;
+
+    //iterate elements to call down and place each element
+    for I := 0 to Pred(Elements.Count) do
+    begin
+      LElement := Elements[I];
+      FBounds.IndexOf(LElement, J);
+
+      //only call the place method when there is a bounds provided, otherwise
+      //the caller wants the default placement
+      if J >= 0 then
+      begin
+        LBound := FBounds.Items[J] as INyxProportionalBounds;
+
+        //if we can't place an element bail with the error
+        if not DoPlaceElement(LElement, LBound, Error) then
+          Exit;
+      end;
+    end;
+
+    //success
+    Result := True;
+  except on E : Exception do
+    Error := E.Message;
+  end;
+end;
+
+function TNyxLayoutProportionalImpl.GetBounds(const AElement: INyxElement): INyxProportionalBounds;
+var
+  I: Integer;
+begin
+  Result := nil;
+
+  //we use an element collection to store bounds with the same id as the element
+  //so we can use the input as lookup
+  FBounds.IndexOf(AElement, I);
+
+  if not I >= 0 then
+    Exit;
+
+  Result := FBounds[I] as INyxProportionalBounds;
+end;
+
+function TNyxLayoutProportionalImpl.Add(const AElement: INyxElement;
+  const ABounds: INyxProportionalBounds): INyxLayoutProportional;
+begin
+  Result := Self as INyxLayoutProportional;
+
+  //add the element normally
+  Add(AElement);
+
+  //when the element is valid, update the id and add
+  if Assigned(AElement) and Assigned(ABounds) then
+  begin
+    ABounds.ID := AElement.ID; //used for lookup
+    FBounds.Add(ABounds);
+  end;
+end;
+
+function TNyxLayoutProportionalImpl.Remove(const AElement: INyxElement): INyxLayoutProportional;
+begin
+  Result := Self as INyxLayoutProportional;
+  Elements.Delete(AElement);
+end;
+
+constructor TNyxLayoutProportionalImpl.Create;
+begin
+  inherited Create;
+  FBounds := NewNyxElements;
+
+  //add an observer to remove the bounds when an element is removed
+  Elements.Observe(@RemoveBounds, FRemoveBoundsID);
+end;
+
+destructor TNyxLayoutProportionalImpl.Destroy;
+begin
+  //remove the observer since we're about to clear the bounds
+  Elements.RemoveObserver(FRemoveBoundsID);
+  FBounds.Clear;
+  FBounds := nil;
+  inherited Destroy;
+end;
+
+{ TNyxProportionalBoundsImpl }
+
+function TNyxProportionalBoundsImpl.GetLeft: Single;
+begin
+  Result := FLeft;
+end;
+
+function TNyxProportionalBoundsImpl.GetTop: Single;
+begin
+  Result := FTop;
+end;
+
+procedure TNyxProportionalBoundsImpl.SetLeft(const AValue: Single);
+begin
+  FLeft := AValue;
+end;
+
+procedure TNyxProportionalBoundsImpl.SetTop(const AValue: Single);
+begin
+  FTop := AValue;
+end;
+
+function TNyxProportionalBoundsImpl.UpdateLeft(const AValue: Single): INyxProportionalBounds;
+begin
+  Result := Self as INyxProportionalBounds;
+  Left := AValue;
+end;
+
+function TNyxProportionalBoundsImpl.UpdateTop(const AValue: Single): INyxProportionalBounds;
+begin
+  Result := Self as INyxProportionalBounds;
+  Top := AValue;
+end;
+
+constructor TNyxProportionalBoundsImpl.Create;
+begin
+  inherited Create;
+  FLeft := 0;
+  FTop := 0;
 end;
 
 { TNyxBoundsImpl }
